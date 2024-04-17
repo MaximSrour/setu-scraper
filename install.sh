a57ef6585dd69ed2575216bf8f59509e5f59fe7d
Maxim Srour <maxim.srour@gmail.com>
2024-04-17 11:10:28 +1000

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo apt-get update
-sudo apt-get install firefox
-firefox --version
+sudo apt update
+sudo apt install firefox
+
 wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz
 tar -xvzf geckodriver*
 chmod +x geckodriver
